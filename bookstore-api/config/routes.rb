707976e2f91f72dbb6c5b92611be6b93085710a1
Brena Monteiro <brena@imusics.com>
2020-09-27 00:17:17 -0300

    Change book route to create by webhook.

--- bookstore-api/config/routes.rb
+++ bookstore-api/config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  resources :books
   resources :publishing_houses, path: '/publishing-houses'
+
+  resources :books, only: [:index, :show] do
+    post :webhook, on: :collection
+  end
 
   resources :authors, only: [:index, :show] do
     post :webhook, on: :collection
--- bookstore-api/spec/routing/books_spec.rb
+++ bookstore-api/spec/routing/books_spec.rb
@@ -11,15 +11,7 @@ RSpec.describe 'routes to the BooksController', type: :routing do
     it { should route(:get, "/books/#{id}").to(controller: :books, action: :show, id: id) }
   end
 
-  describe 'POST create' do
-    it { should route(:post, '/books').to(controller: :books, action: :create) }
-  end
-
-  describe 'PATCH/PUT show' do
-    it { should route(:put, "/books/#{id}").to(controller: :books, action: :update, id: id) }
-  end
-
-  describe 'DELETE destroy' do
-    it { should route(:delete, "/books/#{id}").to(controller: :books, action: :destroy, id: id) }
+  describe 'POST webhook' do
+    it { should route(:post, '/books/webhook').to(controller: :books, action: :webhook) }
   end
 end
