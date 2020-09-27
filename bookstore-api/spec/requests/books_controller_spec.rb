0f6951a81fe09add6dbd215ca4db063202f53c35
Brena Monteiro <brena@imusics.com>
2020-09-27 01:45:19 -0300

    Implement new routes to book.

--- bookstore-api/spec/requests/books_controller_spec.rb
+++ bookstore-api/spec/requests/books_controller_spec.rb
@@ -2,11 +2,22 @@ require 'rails_helper'
 
 RSpec.describe BooksController, type: :request do
   let(:id)            { 1 }
-  let(:price)         { [30.0, 50.0, 100.0].sample }
   let(:no_result)     { { data: [], meta: { total: 0 } }.to_json }
-  let(:author)        { Author.create(name: 'Simone de Beauvoir') }
+  let(:author)        { Author.create(name: 'Alain de Botton') }
   let(:publisher)     { PublishingHouse.create(name: 'Hogarth Press') }
-  let(:error_result)  { { error: "Couldn't find Book with 'id'=#{id}" }.to_json }
+
+  let(:error_result)         { { error: "Couldn't find Book" }.to_json }
+  let(:error_result_author)  { { error: "Couldn't find Author" }.to_json }
+
+  let(:payload_created_no_name) { { action: "created", comment: { body: "" }, issue: { title: author.name } }.to_json }
+  let(:payload_created)         { { action: "created", comment: { body: 'The Architecture of Happiness' }, issue: { title: author.name } }.to_json }
+
+  let(:payload_edited_no_name) { { action: "edited", comment: { body: "" }, issue: { title: author.name }, changes: { body: { from: 'The Architecture of Happiness' } } }.to_json }
+  let(:payload_edited)         { { action: "edited", comment: { body: 'The Architecture of Happiness' }, issue: { title: author.name }, changes: { title: { from: "The News: A User's Manual" } } }.to_json }
+
+  let(:payload_deleted_no_name)   { { action: "deleted", comment: { body: "" }, issue: { title: author.name } }.to_json }
+  let(:payload_deleted_no_author) { { action: "deleted", comment: { body: "The Architecture of Happiness" }, issue: { title: 'Virginia Wolf' } }.to_json }
+  let(:payload_deleted)           { { action: "deleted", comment: { body: 'The Architecture of Happiness' }, issue: { title: author.name } }.to_json }
 
   describe 'GET index' do
     it 'returns a 200 and no book' do
@@ -17,7 +28,7 @@ RSpec.describe BooksController, type: :request do
     end
 
     it 'returns a 200 and book list' do
-      Book.create(title: 'Alain de Botton', price: price, author: author, publisher: publisher)
+      Book.create(title: 'The Architecture of Happiness', author: author, publisher: publisher)
 
       get '/books'
 
@@ -28,7 +39,7 @@ RSpec.describe BooksController, type: :request do
 
   describe 'GET show' do
     it 'returns a 200 and the book' do
-      Book.create(title: 'Alain de Botton', price: price, author: author, publisher: publisher)
+      Book.create(title: 'The Architecture of Happiness', author: author, publisher: publisher)
 
       get "/books/#{id}"
 
@@ -44,69 +55,82 @@ RSpec.describe BooksController, type: :request do
     end
   end
 
-  describe 'POST create' do
-    it 'returns a 201 and the book' do
-      post '/books', params: { book: { title: 'Virginia Woolf', price: price, author_id: author.id, publisher_id: publisher.id, publisher_type: publisher.class.name } }
+  describe 'POST webhook' do
+    describe '.opened' do
+      it 'returns a 201 and the book' do
+        post "/books/webhook", params: { payload: payload_created }
 
-      expect(response).to have_http_status(:created)
-      expect(response.body).to_not eql(no_result)
+        expect(response).to have_http_status(:created)
+        expect(response.body).to_not eql(no_result)
+      end
+
+      it 'returns a 422 and error' do
+        book = Book.new(author: author, publisher: publisher)
+        book.valid?
+
+        post "/books/webhook", params: { payload: payload_created_no_name }
+
+        expect(response).to have_http_status(:unprocessable_entity)
+        expect(response.body).to eql(book.errors.messages.to_json)
+      end
     end
 
-    it 'returns a 422 and error' do
-      book = Book.new
-      book.valid?
+    describe '.edited' do
+      it 'returns a 200 and the book' do
+        Book.create(title: 'The Architecture of Happiness', author: author, publisher: publisher)
 
-      post '/books', params: { book: { title: '' } }
+        post '/books/webhook', params: { payload: payload_edited }
 
-      expect(response).to have_http_status(:unprocessable_entity)
-      expect(response.body).to eql(book.errors.messages.to_json)
-    end
-  end
+        expect(response).to have_http_status(:ok)
+        expect(response.body).to_not eql(no_result)
+      end
 
-  describe 'PATCH/PUT update' do
-    it 'returns a 200 and the book' do
-      Book.create(title: 'Alain de Botton', price: price, author: author, publisher: publisher)
+      it 'returns a 422 and error' do
+        Book.create(title: 'The Architecture of Happiness', author: author, publisher: publisher)
+        book = Book.new(author: author, publisher: publisher)
+        book.valid?
 
-      put "/books/#{id}", params: { book: { title: 'Simone de Beauvoir' } }
+        post '/books/webhook', params: { payload: payload_edited_no_name }
 
-      expect(response).to have_http_status(:ok)
-      expect(response.body).to_not eql(no_result)
-    end
+        expect(response).to have_http_status(:unprocessable_entity)
+        expect(response.body).to eql(book.errors.messages.to_json)
+      end
 
-    it 'returns a 422 and error' do
-      Book.create(title: 'Alain de Botton', price: price, author: author, publisher: publisher)
-      book = Book.new
-      book.valid?
+      it 'returns a 404 and error' do
+        post '/books/webhook', params: { payload: payload_edited_no_name }
 
-      put "/books/#{id}", params: { book: { title: '', price: '', author_id: '', publisher_id: '' } }
-
-      expect(response).to have_http_status(:unprocessable_entity)
-      expect(response.body).to eql(book.errors.messages.to_json)
+        expect(response).to have_http_status(:not_found)
+        expect(response.body).to eql(error_result)
+      end
     end
 
-    it 'returns a 404 and error' do
-      put "/books/#{id}", params: {}
+    describe '.deleted' do
+      it 'returns a 204 and no book' do
+        Book.create(title: 'The Architecture of Happiness', author: author, publisher: publisher)
+        
+        post "/books/webhook", params: { payload: payload_deleted }
+        
+        expect(response).to have_http_status(:no_content)
+        expect(response.body).to_not eql(no_result)
+      end
 
-      expect(response).to have_http_status(:not_found)
-      expect(response.body).to eql(error_result)
-    end
-  end
+      it 'returns a 404 and error because not found the book' do
+        Book.create(title: 'The Architecture of Happiness', author: author, publisher: publisher)
 
-  describe 'DELETE destroy' do
-    it 'returns a 204 and no book' do
-      Book.create(title: 'Alain de Botton', price: price, author: author, publisher: publisher)
+        post "/books/webhook", params: { payload: payload_deleted_no_name }
 
-      delete "/books/#{id}"
+        expect(response).to have_http_status(:not_found)
+        expect(response.body).to eql(error_result)
+      end
 
-      expect(response).to have_http_status(:no_content)
-      expect(response.body).to_not eql(no_result)
-    end
+      it 'returns a 404 and error because not found author' do
+        Book.create(title: 'The Architecture of Happiness', author: author, publisher: publisher)
 
-    it 'returns a 404 and error' do
-      delete "/books/#{id}"
+        post "/books/webhook", params: { payload: payload_deleted_no_author }
 
-      expect(response).to have_http_status(:not_found)
-      expect(response.body).to eql(error_result)
+        expect(response).to have_http_status(:not_found)
+        expect(response.body).to eql(error_result_author)
+      end
     end
   end
 end
